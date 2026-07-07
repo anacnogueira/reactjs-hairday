f2c93a416d0e684f258c6b336290568c43d97afe
Ana Claudia <anacnogueira@gmail.com>
2026-07-07 17:49:01 -0300

    Adicionando componente de Schedule List

---
+++ src/core-components/period-list.tsx
@@ -0,0 +1,53 @@
+import { MoonStarsIcon, SunHorizonIcon } from "@phosphor-icons/react"
+import { CloudSunIcon } from "@phosphor-icons/react/dist/ssr"
+import { Icon } from "../components/icon"
+import Text from "../components/text"
+
+const periods = {
+    morning: {
+        title: "Manhã",
+        icon: SunHorizonIcon,
+        time: "9h-12h"
+    },
+    afternoon: {
+        title: "Tarde",
+        icon: CloudSunIcon,
+        time: "13h-18h"
+
+    },
+        night: {
+        title: "Noite",
+        icon: MoonStarsIcon,
+        time: "19h-21h"
+
+    }
+}
+
+interface PeriodListProps {
+    period: keyof typeof periods;
+    children: React.ReactNode;
+}
+
+
+export default function PeriodList({period, children}: PeriodListProps) {
+    
+    return (
+        <section className="border rounded-lg border-gray-600">
+            <div className="px-5 py-3 flex items-center justify-between gap-6 border-b border-gray-600">
+                <div className="flex items-center gap-2">
+                    <Icon svg={periods[period].icon} className="fill-yellow size-5" />
+                    <Text as="h3" variant="text-sm" className="text-gray-300">
+                        {periods[period].title}
+                    </Text>
+                </div>
+
+                <Text className="text-gray-400" variant="text-md">
+                     {periods[period].title}
+                </Text>
+            </div>
+            <ul className="flex flex-col p-5 gap-0.5">
+                {children}
+            </ul>
+        </section>
+    )
+}
--- src/core-components/schedule.tsx
+++ src/core-components/schedule.tsx
@@ -1,3 +1,4 @@
+import PeriodList from "./period-list";
 import ScheduleHeader from "./schedule-header";
 
 export default function Schedule() {
@@ -5,6 +6,19 @@ export default function Schedule() {
         <div className="w-full py-20">
             <div className="mx-auto flex flex-col gap-8 max-w-170.5">
                 <ScheduleHeader />
+                <div className="space-y-3">
+                    <PeriodList period="morning">
+                        Item
+                    </PeriodList>
+
+                    <PeriodList period="afternoon">
+                        Item
+                    </PeriodList>
+
+                    <PeriodList period="night">
+                        Item
+                    </PeriodList>
+                </div>
             </div>
             
         </div>
