4ef3a2ea041d78333addc1f9578d2c57dea22bfa
Ana Claudia <anacnogueira@gmail.com>
2026-07-07 17:16:03 -0300

    Adicionando componente de Schedule Header

--- src/core-components/schedule.tsx
+++ src/core-components/schedule.tsx
@@ -1,10 +1,12 @@
+import ScheduleHeader from "./schedule-header";
+
 export default function Schedule() {
     return (
         <div className="w-full py-20">
             <div className="mx-auto flex flex-col gap-8 max-w-170.5">
-
+                <ScheduleHeader />
             </div>
-            Agendamentos
+            
         </div>
     )
 }
